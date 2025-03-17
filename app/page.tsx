b18a228037450feae7115afcb1c005c04f13dd93
Maryna Karpova <mari.artarea@gmail.com>
2025-03-17 15:32:56 +0100

    page app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Header from '@/components/shared/header';
 
