1ae23770048770f9c7313120ff045c0fa67c27a1
Maryna Karpova <mari.artarea@gmail.com>
2025-06-17 09:57:48 +0200

    fix

--- app/[role]/user/cart/page.tsx
+++ app/[role]/user/cart/page.tsx
@@ -19,7 +19,6 @@ export default function CartPage() {
   );
 
   const handlePayment = () => {
-    // Tu możesz dodać logikę przejścia do płatności
     alert('Przejście do płatności - do zaimplementowania');
   };
 
