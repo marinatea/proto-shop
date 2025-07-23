04b898ccafd413025393f32f460e2149d93889fa
Maryna Karpova <mari.artarea@gmail.com>
2025-07-23 11:48:04 +0200

    edit

--- components/public-page/templateCard.tsx
+++ components/public-page/templateCard.tsx
@@ -76,7 +76,7 @@ const TemplateCard = ({
           </div>
         )}
 
-        {isOwner && (
+        {isOwner && session?.user && (
           <Link
             href={`/user/user/products/${id}/edit`}
             className="absolute top-2 right-2 text-gray-500 hover:text-black z-10"
